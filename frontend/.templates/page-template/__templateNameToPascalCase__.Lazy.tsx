23c6893882fef2874aeecc89104cc12d0387fbfb
nalben <baroqueworks64502@gmail.com>
2025-11-22 13:44:50 +0300

    folder

--- frontend/.templates/page-template/__templateNameToPascalCase__.Lazy.tsx
+++
@@ -1,3 +0,0 @@
-import React, { lazy } from 'react';
-
-export const Lazy__templateNameToPascalCase__ = lazy(() => import('./__templateNameToPascalCase__'));
--- frontend/.templates/page-template/index.ts
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export {Lazy__templateNameToPascalCase__ as __templateNameToPascalCase__} from './__templateNameToPascalCase__.Lazy';
