23bacb1dbf9389d82d160b15e6284851e1f0eee2
nalben <baroqueworks64502@gmail.com>
2025-11-28 11:34:12 +0300

    buildwebpack, components, axiosinstance

--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -2,7 +2,6 @@
 import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/";
-// const API_URL = process.env.REACT_APP_API_URL || "http://77.105.129.150:3001";
 // const API_URL = process.env.REACT_APP_API_URL || "/api";
 
 const axiosInstance = axios.create({
