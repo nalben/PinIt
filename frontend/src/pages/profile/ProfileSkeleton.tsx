80cefc3f542edfa5a9da565a7648249b4830061c
nalben <baroqueworks64502@gmail.com>
2026-02-09 16:53:23 +0300

    profile skeleton render fix

--- frontend/src/pages/profile/ProfileSkeleton.tsx
+++ frontend/src/pages/profile/ProfileSkeleton.tsx
@@ -6,7 +6,7 @@ type ProfileSkeletonProps = {
 };
 
 const ProfileSkeleton = ({ isOwner }: ProfileSkeletonProps) => {
-  return (
+  return isOwner ? (
     <div className={classes.profile}>
       <div className={`${classes.avatar_con} ${classes.skeleton} ${classes.skeleton_avatar}`} />
       <div className={classes.profile_username}>
@@ -16,9 +16,25 @@ const ProfileSkeleton = ({ isOwner }: ProfileSkeletonProps) => {
           <div className={`${classes.skeleton} ${classes.skeleton_icon}`} />
           <div className={`${classes.skeleton} ${classes.skeleton_line_xs}`} />
         </div>
-        {isOwner && (
+        <div className={`${classes.skeleton} ${classes.skeleton_line_xs}`} />
+      </div>
+      <div className={classes.friends}>
+        <div className={classes.interact_btns}>
+          <div className={`${classes.skeleton} ${classes.skeleton_btn}`} />
+          <div className={`${classes.skeleton} ${classes.skeleton_btn}`} />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className={classes.profile}>
+      <div className={`${classes.avatar_con} ${classes.skeleton} ${classes.skeleton_avatar}`} />
+      <div className={classes.profile_username}>
+        <div className={`${classes.skeleton} ${classes.skeleton_line}`} />
+        <div className={`${classes.skeleton} ${classes.skeleton_line_sm}`} />
+        <div className={classes.skeleton_row}>
+          <div className={`${classes.skeleton} ${classes.skeleton_icon}`} />
           <div className={`${classes.skeleton} ${classes.skeleton_line_xs}`} />
-        )}
+        </div>
       </div>
       <div className={classes.friends}>
         <div className={classes.interact_btns}>
